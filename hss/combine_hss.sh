d0dae19c0dde333bb41366ee3aa22bf910eb19a2
Stefan Schuh <stefan.schuh.ba@gmail.com>
2017-12-09 22:25:48 +0100

    added combine_hss.sh

---
+++ hss/combine_hss.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Dieses Script kombiniert einzelne Dateien aus dem Hochschulschriften-Workflow
+# zu einer Eingabedatei für Alma
+
+# bind header to HEAD
+read -d '' HEAD <<EOF
+<?xml version="1.0" encoding="UTF-8" ?>
+<marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">
+EOF
+
+# bind footer to FOOT
+FOOT='</marc:collection>'
+
+# read contents of all files to variable BODY
+BODY=`sed -e '1,2d' \
+           -e '$d' \
+           -e '/^.*tag="008"/s/[a-z]m/ m/' \
+           *.xml`
+
+# combine to one file
+echo -e "$HEAD\n$BODY\n$FOOT" > out.xml
